10022def7f2c06bf9848c0db58378f7ec040d77c
aholland909 <andrewholland909@gmail.com>
2023-04-06 16:38:10 +0100

    feat: added CV download

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import Head from "next/head";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleArrowDown,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 import gql from "graphql-tag";
 import apolloClient from "@/utils/apollo-client";
 import { formatPublishedDateForDisplay } from "@/utils/date";
 
 export default function Home(props) {
-  const { projects } = props;
+  const { projects, cv } = props;
   return (
     <>
       <Head>
@@ -21,14 +24,14 @@ export default function Home(props) {
       <main className="flex flex-col justify-center align-center items-center pt-0 p-10 ">
         <section className="pb-10">
           <div className="flex flex-col text-center items-center">
-            <div className="p-4">
+            <div className="pt-0 p-4">
               <h1 className="text-3xl font-bold">Hi! I&apos;m Andy Holland</h1>
               <h2 className="text-xl text-gray-700">
                 A Fullstack Developer, passionate about accessibility and cars!
                 🚗
               </h2>
             </div>
-            <div className="flex items-center justify-center p-4">
+            <div className="flex items-center justify-center">
               <Link href="https://github.com/aholland909">
                 <FontAwesomeIcon className="fa-xl p-2" icon={faGithub} />
               </Link>
@@ -56,6 +59,15 @@ export default function Home(props) {
                 className=""
               />
             </div>
+            <Link
+              href={cv.url}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="font-general-medium flex justify-evenly items-center w-40 my-0 sm:my-6 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-600 text-gray-500 hover:text-white duration-500"
+            >
+              <FontAwesomeIcon icon={faCircleArrowDown} />
+              Download CV
+            </Link>
           </div>
         </section>
         <section className="flex flex-col items-center">
@@ -71,14 +83,16 @@ export default function Home(props) {
                   className="flex flex-1 flex-col max-w-sm rounded-md overflow-hidden shadow-lg"
                 >
                   <Image
-                    className="w-full h-48 object-cover"
+                    className="w-full h-48 object-contain"
                     src={project.thumbnail.url}
                     height={300}
                     width={300}
                     alt={project.slug}
                   />
                   <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{project.title}</div>
+                    <div className="font-bold text-xl mb-2">
+                      {project.title}
+                    </div>
                     <p className="text-gray-700 text-base">{project.excerpt}</p>
                   </div>
                   <div className="px-6 py-4 mt-auto flex-none">
@@ -118,6 +132,12 @@ export async function getStaticProps({ preview = false }) {
             date
           }
         }
+        assetCollection(where: { title: "Andrew Holland CV" }, limit: 1) {
+          items {
+            title
+            url
+          }
+        }
       }
     `,
   });
@@ -126,6 +146,7 @@ export async function getStaticProps({ preview = false }) {
     props: {
       preview,
       projects: data.projectsCollection.items,
+      cv: data.assetCollection.items[0],
     },
   };
 }
