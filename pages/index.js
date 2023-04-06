ce432e34e426a08cc30c710bc6b5cfb7b1c38e84
aholland909 <andrewholland909@gmail.com>
2023-04-06 15:39:21 +0100

    feat: new projects showcase styling

--- pages/index.js
+++ pages/index.js
@@ -18,15 +18,12 @@ export default function Home(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col justify-center align-center items-center p-10 ">
+      <main className="flex flex-col justify-center align-center items-center pt-0 p-10 ">
         <section className="pb-10">
           <div className="flex flex-col text-center items-center">
             <div className="p-4">
-              <h1 className="text-3xl font-bold">
-                Hi! 👋 I&apos;m Andy Holland
-              </h1>
-              <h2 className="text-xl">
+              <h1 className="text-3xl font-bold">Hi! I&apos;m Andy Holland</h1>
+              <h2 className="text-xl text-gray-700">
                 A Fullstack Developer, passionate about accessibility and cars!
                 🚗
               </h2>
@@ -65,27 +62,36 @@ export default function Home(props) {
           <h2 className="text-2xl font-bold" id="projects">
             Projects
           </h2>
-          <div className="md:flex">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             {projects.map((project) => {
               return (
                 <Link
-                  className="flex flex-col flex-grow p-4 m-4 rounded text-start bg-gray-100"
                   key={project.slug}
                   href={`projects/${project.slug}`}
+                  className="flex flex-1 flex-col max-w-sm rounded-md overflow-hidden shadow-lg"
                 >
-                  <div className="flex flex-1 flex-col">
-                    <Image
-                      className="object-contain h-48 w-48 rounded"
-                      src={project.thumbnail.url}
-                      height={200}
-                      width={200}
-                      alt={project.slug}
-                    />
-                    <h3 className="text-xl pt-4">{project.title}</h3>
-                    <div className="mt-2 mb-4">{project.excerpt}</div>
-                    <div className="mt-auto flex-none">
+                  <Image
+                    className="w-full h-48 object-cover"
+                    src={project.thumbnail.url}
+                    height={300}
+                    width={300}
+                    alt={project.slug}
+                  />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{project.title}</div>
+                    <p className="text-gray-700 text-base">{project.excerpt}</p>
+                  </div>
+                  <div className="px-6 py-4 mt-auto flex-none">
+                    <div className="w-fit bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                       {formatPublishedDateForDisplay(project.date)}
                     </div>
+                    {/* <Link
+                        key={project.slug}
+                        href={`projects/${project.slug}`}
+                        className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full mt-2 transition duration-200 ease-in-out"
+                      >
+                        View Project
+                      </Link> */}
                   </div>
                 </Link>
               );
